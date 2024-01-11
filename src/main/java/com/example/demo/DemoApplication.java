d7fe02b8c91ef40efe7a10a814073f03ede5dbb4
Tran Huu Nhan <huunhantran10@gmail.com>
2024-01-11 21:49:52 +0700

    add upload file

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
+import com.example.demo.service.StorageProperties;
+import com.example.demo.service.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
+@EnableConfigurationProperties(StorageProperties.class)
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+//            storageService.deleteAll();
+            storageService.init();
+        };
+    }
 }
